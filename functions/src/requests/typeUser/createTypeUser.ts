4afd0dcec9cf0bd5c98ec6a87de72b1691d12f8c
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-10-24 09:56:42 -0600

    agregando validaciones necesarias para el body

--- functions/src/requests/typeUser/createTypeUser.ts
+++ functions/src/requests/typeUser/createTypeUser.ts
@@ -1,15 +1,23 @@
 import type { Request, Response } from 'express';
 import { z } from 'zod';
+import { RepeatedName } from './utils/RepeatedName';
 import { db } from '../../db/database';
 
 const schema = z.object({
     name: z.string(),
     description: z.string(),
-    color: z.string(),
+    color: z.string().regex(/#[a-fA-F0-9]{6}/),
 });
 
 const func =async (req: Request, res: Response) => {
     const { name, description, color } = req.payloadData as z.infer<typeof schema> 
+
+    const repeatedName = await RepeatedName(name);
+
+    if (repeatedName) {
+        res.status(400).json({ ok: false, error: 'El nombre ya existe' });
+        return;
+    }
 
     const [result] = await db
         .insertInto('user_types')
