7433b1c290ebf3c2104c2547790cb470d70ec5a2
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-10-24 10:05:51 -0600

    agregando validacion al tipo de usuario

--- functions/src/requests/typeUser/deleteTypeUser.ts
+++ functions/src/requests/typeUser/deleteTypeUser.ts
@@ -3,31 +3,27 @@ import { z } from 'zod';
 import { db } from '../../db/database';
 
 const schema = z.object({
-    id: z.string(),
+    id: z.string().uuid(),
 });
 
 const func = async (req: Request, res: Response) => {
     const { id } = req.payloadData as z.infer<typeof schema>;
-    try {
-        const [typeUser] = await db.selectFrom('user_types')
-            .select(['id', 'name'])
+    
+    const typeUser = await db.selectFrom('user_types')
+        .select(['id', 'name'])
+        .where('id', '=', id)
+        .execute()
+
+    if (typeUser.length === 0) {
+        res.status(404).json({ ok: false, error: 'Tipo de Usuario no encontrado' })
+        return
+    } else {
+        await db
+            .deleteFrom('user_types')
             .where('id', '=', id)
+            .returning(['id', 'name'])
             .execute()
-
-        if (!typeUser.id) {
-            res.status(404).json({ ok: false, error: 'Tipo de Usuario no encontrado' })
-        } else {
-            await db
-                .deleteFrom('user_types')
-                .where('id', '=', typeUser.id)
-                .returning(['id', 'name'])
-                .execute()
-
-
-            res.status(200).json({ ok: true, message: 'Se elimino con exito' })
-        }
-    } catch (error) {
-        res.status(500).json({ ok: false, error: 'El id es erroneo' })
+        res.status(200).json({ ok: true, message: 'Se elimino con exito' })
     }
 }
 
