691e662466838d989f5a7b571e2cfbd749ff3058
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-10-24 10:42:16 -0600

    agregando validacion de poder tomar todos o activos o inactivos

--- functions/src/requests/typeUser/selectTypeUser.ts
+++ functions/src/requests/typeUser/selectTypeUser.ts
@@ -1,10 +1,26 @@
 import type { Request, Response } from 'express';
+import { z } from 'zod'
 import { db } from '../../db/database';
 
+const schema = z.object({
+    option: z.enum(['all', 'active', 'inactive']),
+});
+
+
 const func = async (req: Request, res: Response) => {
-    const typeUser = await db.selectFrom('user_types').selectAll().execute()
+    const { option }  = req.payloadData as z.infer<typeof schema>
+
+    let typeUser;
+    if (option === 'all') {
+        typeUser = await db.selectFrom('user_types').selectAll().execute()
+    }else if (option === 'active') {
+        typeUser = await db.selectFrom('user_types').selectAll().where('deleted_at', 'is', null).execute()
+    }
+    else{
+        typeUser = await db.selectFrom('user_types').selectAll().where('deleted_at', 'is not', null).execute()
+    }
 
     res.json({ typeUser, ok: true });
 }
 
-export const selectTypeUser = { func }
+export const selectTypeUser = { func, schema }
