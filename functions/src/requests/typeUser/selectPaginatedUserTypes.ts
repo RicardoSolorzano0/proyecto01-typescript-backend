d81c50ca2da652e3bd2cfd54576ec35f8b0fddaf
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-01 09:32:04 -0600

    agregando el filtro en tipo de usuario

--- functions/src/requests/typeUser/selectPaginatedUserTypes.ts
+++ functions/src/requests/typeUser/selectPaginatedUserTypes.ts
@@ -6,10 +6,12 @@ const schema = z.object({
     option: z.enum(['all', 'active', 'inactive']),
     limit: z.coerce.number(),
     page: z.coerce.number(),
+    name: z.string().optional(),
 });
 
 const func = async (req: Request, res: Response) => {
-    const { option, limit, page }  = req.payloadData as z.infer<typeof schema>
+    const { option, limit, page, name }  = req.payloadData as z.infer<typeof schema>
+
     const offset = (page-1) * limit;
 
     const db = constructDB();
@@ -18,6 +20,7 @@ const func = async (req: Request, res: Response) => {
         .selectFrom('user_types')
         .$if(option === 'active', (qb) => qb.where('deleted_at', 'is', null))
         .$if(option === 'inactive', (qb) => qb.where('deleted_at', 'is not', null))
+        .$if( name !== '', (qb) => qb.where('name', 'ilike', `%${name!}%`));
 
     //haciendo consulta con paginacion
     const data = await baseQuery
