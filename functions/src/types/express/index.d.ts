ab5a7342b7a6abf26ee1a122c795e9ad0a800041
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-12 10:51:19 -0600

    tipado para express

--- functions/src/types/express/index.d.ts
+++ functions/src/types/express/index.d.ts
@@ -1,5 +1,15 @@
-declare namespace Express {
-  export interface Request {
-    payloadData?:  Record<string, unknown>;
-  }
+import { auth }             from 'firebase-admin';
+import type { RoleType }    from '@/types';
+
+declare global {
+    namespace Express {
+        import DecodedIdToken = auth.DecodedIdToken;
+
+        export interface Request {
+            user?: DecodedIdToken & {
+                [k in RoleType]?: boolean
+            };
+            payloadData?: Record<string, unknown>;
+        }
+    }
 }
